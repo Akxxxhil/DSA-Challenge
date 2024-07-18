baa669192b32228599b86b9e88035faa173c2ebe
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-07-18 08:05:48 +0530

    updated

--- Linked List/MidlleLinkedList.java
+++ Linked List/MidlleLinkedList.java
@@ -1,20 +1,4 @@
 
-public class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode() {
-    }
-
-    ListNode(int val) {
-        this.val = val;
-    }
-
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
-    }
-}
 
 class Solution {
     public ListNode middleNode(ListNode head) {
