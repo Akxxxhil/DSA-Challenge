95fe7719fad246bc2d2d47e01a3071fad2cee72e
Akxxxhil <1941012872.e.anshumanbiswal@gmail.com>
2024-09-10 18:59:10 +0530

    updated

---
+++ Array/Roatate.java
@@ -0,0 +1,14 @@
+public class Roatate {
+
+    public static void rotateArray(int arr[],int k){
+        int ans[]=new int [arr.length];
+        
+
+    }
+    public static void main(String[] args) {
+        int arr[]={10,20,30,40,50,60,70};
+        int k=2;
+        rotateArray(arr, k);
+
+    }
+}
